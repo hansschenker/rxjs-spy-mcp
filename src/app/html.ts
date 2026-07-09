c35616294f556c9c467d93793b3d36218bc2bee9
Hans Schenker <gian.schenker@gmail.com>
2026-07-09 10:09:04 +0200

    Add safe HTML helpers

---
+++ src/app/html.ts
@@ -0,0 +1,12 @@
+export function escapeHtml(value: unknown): string {
+  return String(value)
+    .replaceAll('&', '&amp;')
+    .replaceAll('<', '&lt;')
+    .replaceAll('>', '&gt;')
+    .replaceAll('"', '&quot;')
+    .replaceAll("'", '&#039;');
+}
+
+export function prettyJson(value: unknown): string {
+  return escapeHtml(JSON.stringify(value, null, 2));
+}
